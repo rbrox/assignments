c13a69de35687a151f711759d6bea9286debab85
Rishav Bhagat <rishavbhagat@Rishavs-MacBook-Air.local>
2024-02-19 20:36:06 +0530

    attempt one

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
-
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+export const DB_URL =
+  "postgresql://rishavsci:9dImk4LYVPDw@ep-still-lake-54571540-pooler.us-east-2.aws.neon.tech/1-week10?sslmode=require";
--- week-10/1-postgres-simple/src/index.ts
+++ week-10/1-postgres-simple/src/index.ts
@@ -1,6 +1,8 @@
-import { Client } from 'pg'
-import { DB_URL } from './config';
+import { Client } from "pg";
+import { DB_URL } from "./config";
 
 export const client = new Client({
-    connectionString: DB_URL
+  connectionString: DB_URL,
 });
+
+client.connect();
